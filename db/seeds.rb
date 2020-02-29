eb9433c6625c0984f584e8b50b68114d121d6ee7
DmitryKachalov <rubyzza.dev@gmail.com>
2020-02-29 23:00:36 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,46 @@
+frontend = Category.create(title: 'Frontend')
+backend = Category.create(title: 'Backend')
+mobile = Category.create(title: 'Mobile Development')
 
+# Users
+
+john = User.create(name: 'John', email: 'john@testmail.org', role: 'user')
+daniel = User.create(name: 'Daniel', email: 'daniel007@yahoomail.org', role: 'user')
+bill = User.create(name: 'Bill', email: 'BillyBo@gmailtest.org', role: 'admin')
+
+# Tests
+
+html = frontend.tests.create(title: 'HTML', level: 0, author: john)
+css = frontend.tests.create(title: 'CSS', level: 1, author: john)
+ruby = backend.tests.create(title: 'Ruby', level: 2, author: bill)
+go_lang = backend.tests.create(title: 'Go', level: 3, author: bill)
+swift = mobile.tests.create(title: 'Swift', level: 4, author: daniel)
+
+# Questions
+
+questions = Question.create(
+    [{ body: 'What is the use of figure tag in HTML 5?', test: html },
+     { body: 'What are the entities in HTML?', test: html },
+     { body: 'In how many ways you can compare Ruby string?', test: ruby },
+     { body: 'What are Ruby arrays and how they can be created?', test: ruby },
+     { body: 'Why should one use Go programming language?', test: go_lang },
+     { body: 'Why should one use Swift programming language in 2018?', test: swift }]
+)
+
+# Answers
+
+Answer.create(
+    [{ body: 'Correct answer to HTML', correct: true, question: questions[0] },
+     { body: 'Incorrect answer to HTML', correct: false, question: questions[0] },
+     { body: 'Correct answer to Ruby', correct: true, question: questions[2] },
+     { body: 'Correct answer to Ruby', correct: false, question: questions[2] }]
+)
+
+# Results
+
+TestPassage.create(
+    [{ user: john, test: html, status: 'Finished' },
+     { user: john, test: css, status: 'Not started' },
+     { user: daniel, test: ruby, status: 'Not finished' },
+     { user: bill, test: go_lang, status: 'Finished' }]
+)
