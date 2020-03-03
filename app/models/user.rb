71ba9f14b2f7cdae457e5101517a456e5f5ceeac
DmitryKachalov <rubyzza.dev@gmail.com>
2020-03-03 20:51:54 +0100

    update method by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def by_level(level)
     Test.joins('join test_passages on test_passages.test_id = tests.id')
-        .where('test_passages.user_id = ? and tests.level = ?', self.id, level)
+        .where(test_passages: { user_id: id }, tests: { level: level })
   end
 end
