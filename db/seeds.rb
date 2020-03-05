8dff4f1b4a7d0843ff6e13620cbef476f20de54e
DmitryKachalov <rubyzza.dev@gmail.com>
2020-03-05 18:47:58 +0100

    fix seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,36 +2,36 @@ categories = Category.create(
     [{ title: 'backend' },
             { title: 'frontend' }])
 
-tests = Test.create!(
-    [{ title: 'HTML', level: 0, category_id: categories[1].id },
-             { title: 'CSS', level: 1, category_id: categories[1].id },
-             { title: 'Ruby', level: 2, category_id: categories[0].id },
-             { title: 'Go', level: 3, category_id: categories[0].id }]
-)
-
-questions = Question.create!(
-    [{ body: 'What is the use of figure tag in HTML 5?', test_id: tests[0].id },
-     { body: 'Which selector allows you to access each element of a web page?', test_id: tests[1].id },
-     { body: 'In how many ways you can compare Ruby string?', test_id: tests[2].id },
-     { body: 'Why should one use Go programming language?', test_id: tests[3].id }]
-)
-
-Answer.create!(
-    [{ body: 'Correct answer to HTML', correct: true, question_id: questions[0].id },
-     { body: 'Incorrect answer to HTML', correct: false, question_id: questions[0].id },
-     { body: 'Correct answer to CSS', correct: true, question_id: questions[1].id },
-     { body: 'Incorrect answer to CSS', correct: false, question_id: questions[1].id },
-     { body: 'Correct answer to Ruby', correct: true, question_id: questions[2].id },
-     { body: 'Incorrect answer to Ruby', correct: false, question_id: questions[2].id },
-     { body: 'Correct answer to Go', correct: true, question_id: questions[3].id },
-     { body: 'Incorrect answer to Go', correct: false, question_id: questions[3].id }]
-)
-
 users = User.create!([{ name: 'admin', email: 'admin@mail.org', role: 'admin' },
                       { name: 'user', email: 'user@mail.org', role: 'user' }])
 
-TestPassage.create!([{ user_id: users[1].id, test_id: tests[0].id, status: 'finish' },
-                     { user_id: users[1].id, test_id: tests[1].id, status: 'finish' },
-                     { user_id: users[1].id, test_id: tests[2].id, status: 'finish' },
-                     { user_id: users[1].id, test_id: tests[3].id, status: 'not started' }])
+tests = Test.create!(
+    [{ title: 'HTML', level: 0, category: categories[1], author: users[0] },
+             { title: 'CSS', level: 1, category: categories[1], author: users[0] },
+             { title: 'Ruby', level: 2, category: categories[0], author: users[0] },
+             { title: 'Go', level: 3, category: categories[0], author: users[0] }]
+)
+
+questions = Question.create!(
+    [{ body: 'What is the use of figure tag in HTML 5?', test: tests[0] },
+     { body: 'Which selector allows you to access each element of a web page?', test: tests[1] },
+     { body: 'In how many ways you can compare Ruby string?', test: tests[2] },
+     { body: 'Why should one use Go programming language?', test: tests[3] }]
+)
+
+Answer.create!(
+    [{ body: 'Correct answer to HTML', correct: true, question: questions[0] },
+     { body: 'Incorrect answer to HTML', correct: false, question: questions[0] },
+     { body: 'Correct answer to CSS', correct: true, question: questions[1] },
+     { body: 'Incorrect answer to CSS', correct: false, question: questions[1] },
+     { body: 'Correct answer to Ruby', correct: true, question: questions[2] },
+     { body: 'Incorrect answer to Ruby', correct: false, question: questions[2] },
+     { body: 'Correct answer to Go', correct: true, question: questions[3] },
+     { body: 'Incorrect answer to Go', correct: false, question: questions[3] }]
+)
+
+TestPassage.create!([{ user: users[1], test: tests[0], status: 'finish' },
+                     { user: users[1], test: tests[1], status: 'finish' },
+                     { user: users[1], test: tests[2], status: 'finish' },
+                     { user: users[1], test: tests[3], status: 'not started' }])
 
