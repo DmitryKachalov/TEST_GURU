0c469192a25d9ebf2aec4ccae1056f4d2c3e1bee
DmitryKachalov <rubyzza.dev@gmail.com>
2020-03-10 23:34:10 +0100

    validate category :title

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,7 @@ class Category < ApplicationRecord
   # одной категории принадлежит 1 и более тестов
   has_many :tests, dependent: :nullify
 
+  validates :title, presence: true
+
   default_scope { order(title: :asc) }
 end
