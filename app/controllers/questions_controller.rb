1fd692ba906daf15634f603a081490d5f5ca3c2f
DmitryKachalov <rubyzza.dev@gmail.com>
2020-03-26 14:33:27 +0100

    create question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,27 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_test
+  before_action :find_test, only: [:new, :create, :index]
   before_action :find_question, only: :show
+  skip_before_action :verify_authenticity_token
 
   def index
     @questions = @test.questions
   end
 
   def show; end
+
+  def new
+    @test
+  end
+
+  def create
+    @question = @test.questions.new(question_params)
+    if @question.save
+      redirect_to test_question_path(@test, @question)
+    else
+      render plain: 'Aborted'
+    end
+  end
 
   private
 
@@ -18,6 +32,12 @@ class QuestionsController < ApplicationController
   def find_question
     @question = Question.find(params[:id])
   end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+
 
 end
 
