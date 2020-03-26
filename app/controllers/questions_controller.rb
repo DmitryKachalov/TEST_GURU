a46c2ac9e5641876289f37f5a692bd2d36f25fc0
DmitryKachalov <rubyzza.dev@gmail.com>
2020-03-26 14:19:51 +0100

    add index: show specific question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,22 @@
 class QuestionsController < ApplicationController
 
   before_action :find_test
+  before_action :find_question, only: :show
 
   def index
-    @questions = @test.questions.all
+    @questions = @test.questions
   end
+
+  def show; end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
 end
