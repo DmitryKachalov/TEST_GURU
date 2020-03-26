6581bcda90564d279983bde1e4538774104644f3
DmitryKachalov <rubyzza.dev@gmail.com>
2020-03-26 14:06:30 +0100

    add index: show all questions test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,17 @@
 class QuestionsController < ApplicationController
 
-  def index
+  before_action :find_test
 
+  def index
+    @questions = @test.questions.all
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
   end
 
 end
+
+
