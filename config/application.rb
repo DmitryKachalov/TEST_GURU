2d98514eda635099ea1d1cdb4ef47e3a29d4724b
DmitryKachalov <rubyzza.dev@gmail.com>
2020-03-30 23:00:44 +0200

    add helper update views test

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module TESTGuru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Madrid'
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = %i[en ru]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
