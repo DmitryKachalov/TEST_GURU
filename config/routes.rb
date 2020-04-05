8eb44aadac8ca374d7955411e57f3ebf3808044d
DmitryKachalov <rubyzza.dev@gmail.com>
2020-04-06 00:08:33 +0200

    scaffold answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   # root to: 'tests#index'
   # get '/tests/:category/:title', to: 'tests#search'
 
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 
 end
