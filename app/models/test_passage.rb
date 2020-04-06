1bb7f14b6e7f37d98248c3c34653fbe6648a6cd2
DmitryKachalov <rubyzza.dev@gmail.com>
2020-04-06 10:49:27 +0200

    add column test_passage

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -2,4 +2,5 @@ class TestPassage < ApplicationRecord
   # соединительная таблица собирает данные какой юзер прошел определенный тест
   belongs_to :test
   belongs_to :user
+  belongs_to :current_question, class_name: 'Question', optional: true
 end
