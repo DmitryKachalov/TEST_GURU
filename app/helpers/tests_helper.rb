17c744ae3e0e285bb741101b4d53e4c645b014a9
DmitryKachalov <rubyzza.dev@gmail.com>
2020-04-12 22:09:06 +0200

    update views

---
+++ app/helpers/tests_helper.rb
@@ -0,0 +1,15 @@
+module TestsHelper
+
+  TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
+
+  def test_level(test)
+    TEST_LEVELS[test.level] || :hero
+    # case test.level
+    # when 0 then 'easy'
+    # when 1 then 'elementary'
+    # when 2 then 'advanced'
+    # when 3 then 'hard'
+    # else 'hero'
+    # end
+  end
+end
