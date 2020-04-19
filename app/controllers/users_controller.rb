c1b2c6e48ce4fe3f72f30e06cba967a2f41fbff5
DmitryKachalov <rubyzza.dev@gmail.com>
2020-04-20 00:53:18 +0200

    user_id on reg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to tests_path
-    else
       #Добавляем в сессию идентификатор пользователя после его регистрации
       session[:user_id] = @user.id
+      redirect_to tests_path
+    else
       render :new
     end
   end
