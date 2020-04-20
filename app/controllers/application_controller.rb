8b7f0c21d8678c97a931314b649396c40399c6ac
DmitryKachalov <rubyzza.dev@gmail.com>
2020-04-20 02:01:05 +0200

    redirect after auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user!
+    cookies[:path] = request.path
     unless current_user
       return redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
     end
