0b9596360c2a014fa9703aad4892e4e54ad80817
DmitryKachalov <rubyzza.dev@gmail.com>
2020-05-04 01:05:03 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
   root 'tests#index'
+
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
   # get '/tests/:category/:title', to: 'tests#search'
 
   # delete :logout, to: 'sessions#destroy'
 
 
-  resources :tests do
+  resources :tests, only: :index do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
     end
@@ -21,4 +22,9 @@ Rails.application.routes.draw do
       get :result
     end
   end
+
+  namespace :admin do
+    resources :tests
+  end
+
 end
