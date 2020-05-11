655befa325d3da9a2f0db9cddd6ed6eaf4279101
DmitryKachalov <rubyzza.dev@gmail.com>
2020-05-12 00:21:08 +0200

    flash and fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  BOOTSTRAP_CLASS = Hash.new('alert alert-danger').update(
+      alert: 'alert alert-danger',
+      notice: 'alert alert-success')
+
+  def flash_message
+    flash.map do |key, value|
+      content_tag :p, value, class: "#{BOOTSTRAP_CLASS[key.to_sym]}"
+    end.join.html_safe
+  end
 end
