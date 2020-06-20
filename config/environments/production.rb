99b4db1a4fd3ccb186c872af954d0673f51cfc69
DmitryKachalov <rubyzza.dev@gmail.com>
2020-06-21 00:37:21 +0200

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,9 +13,10 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
+
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'test-guru-kachalov.herokuapp.com'}
-  #config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:                'smtp.gmail.com',
