c5826c5b5cc752bae9c9460093406d1f5f5f293e
DmitryKachalov <rubyzza.dev@gmail.com>
2020-07-07 01:21:46 +0200

    update

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -27,7 +27,7 @@ class Admin::TestsController < Admin::BaseController
 
   def update
     if @test.update(test_params)
-      redirect_to @test
+      redirect_to admin_test_path
     else
       render :edit
     end
