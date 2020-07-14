62bab437dbd3433a08f6cafac2d7f68a4cd8b549
DmitryKachalov <rubyzza.dev@gmail.com>
2020-07-15 01:31:15 +0200

    add local and update

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -8,6 +8,7 @@ class TestPassage < ApplicationRecord
 
   before_validation :before_validation_set_current_question, on: %i[create update]
   before_update :before_update_test_passed
+  before_update :before_update_check_time_limit
 
   def before_update_test_passed
     self.passed = successfully_completed? if completed?
@@ -21,9 +22,6 @@ class TestPassage < ApplicationRecord
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
 
-    return unless test.time_limited?
-
-    self.current_question = nil if time_left <= 0
     save!
   end
 
@@ -71,5 +69,11 @@ class TestPassage < ApplicationRecord
 
   def questions_collection
     test.questions.order(:id).where('id > ?', current_question.id)
+  end
+
+  def before_update_check_time_limit
+    return unless test.time_limited?
+
+    self.current_question = nil if time_left <= 0
   end
 end
